6ed75534f18685fe9f3ef5bcf1495d89ce77d0a7
Leon Windhorst <leon@windhorst.dev>
2024-05-13 09:36:50 +0200

    Set default_prog to pwsh

--- wezterm.lua
+++ wezterm.lua
@@ -1,17 +1,19 @@
-local wezterm = require('wezterm')
+local wezterm = require("wezterm")
 local mux = wezterm.mux
-local colors = require('lua/rose-pine').colors()
+local colors = require("lua/rose-pine").colors()
 
 return {
-	-- default_domain = 'WSL:Debian',
+	-- default_domain = "WSL:Debian",
+	default_prog = { "pwsh" },
 	enable_tab_bar = false,
 
 	colors = colors,
 	force_reverse_video_cursor = true,
 
-	font = wezterm.font('Cascadia Mono'),
+	font = wezterm.font("Cascadia Mono"),
 	font_size = 14.0,
 
-	window_decorations = 'TITLE',
+	window_decorations = "TITLE",
 	window_background_opacity = 0.95,
 }
+
